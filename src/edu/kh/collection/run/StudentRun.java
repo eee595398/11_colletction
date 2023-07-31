9d9d9fd1228e1af228cab2be0c5d2587218f5fe3
eee595398 <eee595398@gmail.com>
2023-07-31 17:32:51 +0900

    1

---
+++ src/edu/kh/collection/run/StudentRun.java
@@ -0,0 +1,14 @@
+package edu.kh.collection.run;
+
+import edu.kh.collection.model.service.StudenetService;
+
+public class StudentRun {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		StudenetService service = new StudenetService();
+		service.displayMenu();
+	}
+
+}
